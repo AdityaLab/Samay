697c3dcfb420b28bbf9cdf59d5d7aa3cd27dd167
ASudu <sanand315@gatech.edu>
2025-03-05 11:18:41 -0500

    explicitly defining distr_op

--- src/samay/models/uni2ts/model/moirai/module.py
+++ src/samay/models/uni2ts/model/moirai/module.py
@@ -35,6 +35,11 @@ from uni2ts.module.position import (
 )
 from uni2ts.module.transformer import TransformerEncoder
 from uni2ts.module.ts_embed import MultiInSizeLinear
+from uni2ts.distribution.mixture import MixtureOutput
+from uni2ts.distribution.normal import NormalFixedScaleOutput
+from uni2ts.distribution.student_t import StudentTOutput
+from uni2ts.distribution.log_normal import LogNormalOutput
+from uni2ts.distribution.negative_binomial import NegativeBinomialOutput
 
 
 def encode_distr_output(
@@ -127,8 +132,18 @@ class MoiraiModule(
             shared_time_qk_proj=True,
             d_ff=None,
         )
-        self.distr_output = distr_output
-        self.param_proj = self.distr_output.get_param_proj(d_model, patch_sizes)
+        
+        # self.distr_output = distr_output
+        # components is a list of distribution output objects
+        student_t = StudentTOutput()
+        normal_fixed_scale = NormalFixedScaleOutput()
+        negative_binomial = NegativeBinomialOutput()
+        log_normal = LogNormalOutput()
+        self.distr_output = MixtureOutput(components=[student_t, normal_fixed_scale, negative_binomial, log_normal])
+        # args_dim component of self.param_proj has the dimension of parameters
+        # of each component of the mixture distribution
+        self.param_proj = self.distr_output.get_param_proj(in_features=d_model, # hidden dimension
+                                                           out_features=patch_sizes)
 
     def forward(
         self,
