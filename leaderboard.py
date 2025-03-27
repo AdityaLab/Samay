d4c4cc8382d57f220b08490dd3705822e39f354e
ASudu <sanand315@gatech.edu>
2025-03-27 12:36:09 -0400

    leaderboard moirai

--- leaderboard.py
+++ leaderboard.py
@@ -2,7 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-
+import time
 
 # src_path = os.path.abspath(os.path.join("src"))
 # if src_path not in sys.path:
@@ -74,7 +74,8 @@ if __name__ == "__main__":
     # create csv file for leaderboard if not already created
     csv_path = f"leaderboard/{model_name}.csv"
     if not os.path.exists(csv_path):
-        df = pd.DataFrame(columns=["dataset", "mse", "mae", "mase", "mape", "rmse", "nrmse", "smape", "msis", "nd", "mwsq", "crps"])
+        print(f"Creating leaderboard csv file: {csv_path}")
+        df = pd.DataFrame(columns=["dataset", "size_in_MB", "eval_time_in_seconds", "mse", "mae", "mase", "mape", "rmse", "nrmse", "smape", "msis", "nd", "mwsq", "crps"])
         df.to_csv(csv_path, index=False)
 
     # Load model config
@@ -97,72 +98,99 @@ if __name__ == "__main__":
     # Set the datasets to evaluate
     NAMES = ECON_NAMES | SALES_NAMES
 
-    for dataset_name, freqs in NAMES.items():
+    # Sort the datasets based on the dataset size
+    filesizes = []
+    for name, freqs in NAMES.items():
         for freq in freqs:
-            # Adjust the context and prediction length based on the frequency
-            # pred_len, context_len = calc_pred_and_context_len(freq)
-            pred_len, context_len = 96, 512
-            if model_name == "timesfm":
-                args["config"]["horizon_len"] = pred_len
-                args["config"]["context_len"] = context_len
-            elif model_name == "moment":
-                args["config"]["forecast_horizon"] = pred_len
-            elif model_name == "ttm":
-                args["config"]["horizon_len"] = pred_len
-                args["config"]["context_len"] = context_len
-            
-            # Set the dataset path
             if len(freqs) == 1:
-                dataset_path = f"data/gifteval/{dataset_name}/data.csv"
-            else:
-                dataset_path = f"data/gifteval/{dataset_name}/{freq}/data.csv"
-            print(f"Creating leaderboard for dataset: {dataset_name}, context_len: {context_len}, horizon_len: {pred_len}")
+                path = f"data/gifteval/{name}/data.csv"
+            elif len(freqs) == 2:
+                path = f"data/gifteval/{name}/{freq}/data.csv"
             
-            # Initialize the model and dataset
-            if model_name == "timesfm":
-                model = TimesfmModel(**args)
-                dataset = TimesfmDataset(datetime_col='timestamp', path=dataset_path, mode='test', context_len=args["config"]["context_len"], horizon_len=args["config"]["horizon_len"], boundaries=(-1, -1, -1), batchsize=64)
-                metrics = model.evaluate(dataset)
+            size = os.path.getsize(path) / 1e6
+            filesizes.append((name, freq, size))
 
-            elif model_name == "moment":
-                model = MomentModel(**args)
-                args["config"]["task_name"] = "forecasting"
-                train_dataset = MomentDataset(datetime_col='timestamp', path=dataset_path, mode='train', horizon_len=args["config"]["forecast_horizon"], normalize=False)
-                dataset = MomentDataset(datetime_col='timestamp', path=dataset_path, mode='test', horizon_len=args["config"]["forecast_horizon"], normalize=False)
-                finetuned_model = model.finetune(train_dataset, task_name="forecasting")
-                metrics = model.evaluate(dataset, task_name="forecasting")
-                print(metrics)
+    filesizes.sort(key=lambda x: x[2])
 
-            elif model_name == "chronos":
-                model = ChronosModel(**args)
-                dataset_config = load_args("config/chronos_dataset.json")
-                dataset_config["context_length"] = context_len
-                dataset_config["prediction_length"] = pred_len
-                dataset = ChronosDataset(datetime_col='timestamp', path=dataset_path, mode='test', config=dataset_config, batch_size=4)
-                metrics = model.evaluate(dataset, horizon_len=dataset_config["prediction_length"], quantile_levels=[0.1, 0.5, 0.9])
+    for fname, freq, fs in filesizes[9``:]:
+        print(f"Evaluating {fname} ({freq})")
+        # Adjust the context and prediction length based on the frequency
+        # pred_len, context_len = calc_pred_and_context_len(freq)
+        pred_len, context_len = 96, 512
+        if model_name == "timesfm":
+            args["config"]["horizon_len"] = pred_len
+            args["config"]["context_len"] = context_len
+        elif model_name == "moment":
+            args["config"]["forecast_horizon"] = pred_len
+        elif model_name == "ttm":
+            args["config"]["horizon_len"] = pred_len
+            args["config"]["context_len"] = context_len
+        elif model_name == "moirai":
+            args["config"]["horizon_len"] = pred_len
+            args["config"]["context_len"] = context_len
+        
+        # Set the dataset path
+        if len(NAMES.get(fname)) == 1:
+            dataset_path = f"data/gifteval/{fname}/data.csv"
+        else:
+            dataset_path = f"data/gifteval/{fname}/{freq}/data.csv"
+        
+        # Initialize the model and dataset
+        if model_name == "timesfm":
+            model = TimesfmModel(**args)
+            dataset = TimesfmDataset(datetime_col='timestamp', path=dataset_path, mode='test', context_len=args["config"]["context_len"], horizon_len=args["config"]["horizon_len"], boundaries=(-1, -1, -1), batchsize=64)
+            metrics = model.evaluate(dataset)
 
-            elif model_name == "chronosbolt":
-                repo = "amazon/chronos-bolt-small"
-                model = ChronosBoltModel(repo=repo)
-                dataset = ChronosBoltDataset(datetime_col='timestamp', path=dataset_path, mode='test', batch_size=8, context_len=context_len, horizon_len=pred_len)
-                metrics = model.evaluate(dataset, horizon_len=pred_len, quantile_levels=[0.1, 0.5, 0.9])
+        elif model_name == "moment":
+            model = MomentModel(**args)
+            args["config"]["task_name"] = "forecasting"
+            train_dataset = MomentDataset(datetime_col='timestamp', path=dataset_path, mode='train', horizon_len=args["config"]["forecast_horizon"], normalize=False)
+            dataset = MomentDataset(datetime_col='timestamp', path=dataset_path, mode='test', horizon_len=args["config"]["forecast_horizon"], normalize=False)
+            finetuned_model = model.finetune(train_dataset, task_name="forecasting")
+            metrics = model.evaluate(dataset, task_name="forecasting")
+            print(metrics)
 
-            elif model_name == "ttm":
-                model = TinyTimeMixerModel(**args)
-                dataset = TinyTimeMixerDataset(datetime_col='timestamp', path=dataset_path, mode='test', context_len=context_len, horizon_len=pred_len)
-                metrics = model.evaluate(dataset)
-            
-            elif model_name == "moirai":
-                # model = 
-                pass
+        elif model_name == "chronos":
+            model = ChronosModel(**args)
+            dataset_config = load_args("config/chronos_dataset.json")
+            dataset_config["context_length"] = context_len
+            dataset_config["prediction_length"] = pred_len
+            dataset = ChronosDataset(datetime_col='timestamp', path=dataset_path, mode='test', config=dataset_config, batch_size=4)
+            metrics = model.evaluate(dataset, horizon_len=dataset_config["prediction_length"], quantile_levels=[0.1, 0.5, 0.9])
 
-            df = pd.read_csv(csv_path)
-            if dataset_name in df["dataset"].values:
-                df.loc[df["dataset"] == dataset_name, list(metrics.keys())] = list(metrics.values())
-            else:
-                new_row = pd.DataFrame([{**{"dataset": dataset_name}, **metrics}])
-                df = pd.concat([df, new_row], ignore_index=True)
+        elif model_name == "chronosbolt":
+            repo = "amazon/chronos-bolt-small"
+            model = ChronosBoltModel(repo=repo)
+            dataset = ChronosBoltDataset(datetime_col='timestamp', path=dataset_path, mode='test', batch_size=8, context_len=context_len, horizon_len=pred_len)
+            metrics = model.evaluate(dataset, horizon_len=pred_len, quantile_levels=[0.1, 0.5, 0.9])
 
-            df.to_csv(csv_path, index=False)
+        elif model_name == "ttm":
+            model = TinyTimeMixerModel(**args)
+            dataset = TinyTimeMixerDataset(datetime_col='timestamp', path=dataset_path, mode='test', context_len=context_len, horizon_len=pred_len)
+            metrics = model.evaluate(dataset)
+        
+        elif model_name == "moirai":
+            model = MoiraiTSModel(**args)
+            dataset = MoiraiDataset(name=fname,datetime_col='timestamp', freq=freq,
+                                    path=dataset_path, mode='test', context_len=context_len, horizon_len=pred_len)
+
+            start = time.time()
+            metrics = model.evaluate(dataset,leaderboard=True)
+            end = time.time()
+            print(f"Size of dataset: {fs:.2f} MB")
+            print(f"Time taken for evaluation of {fname}: {end-start:.2f} seconds")
+        
+        print("Evaluation done!")
+
+        df = pd.read_csv(csv_path)
+        if fname in df["dataset"].values:
+            df.loc[df["dataset"] == fname, "size_in_MB"] = round(fs,2)
+            df.loc[df["dataset"] == fname, "eval_time_in_seconds"] = round(end-start,2)
+            df.loc[df["dataset"] == fname, list(metrics.keys())] = list(metrics.values())
+        else:
+            new_row = pd.DataFrame([{**{"dataset": fname, "size_in_MB":round(fs,2), "eval_time_in_seconds":round(end-start,2)}, **metrics}])
+            df = pd.concat([df, new_row], ignore_index=True)
+
+        df.to_csv(csv_path, index=False)
 
             
